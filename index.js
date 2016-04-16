ff341b4a5f291735b0b37454619af0c2b0dadd05
Emil Virkki <emil.virkki@gmail.com>
2016-04-16 11:40:58 +0300

    Add Pug templates and session store

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var express = require('express');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 var app = express();
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+var sessionOptions = {
+  ttl: 8*3600,
+  encrypt: true,
+};
+app.use(session({
+  store: new FileStore(sessionOptions),
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
+
 app.get('/', function(req, res) {
-  res.send('Hello JIRA!');
+  res.render('login', { name: 'JIRA' });
 });
 
 app.listen(3000, function() {
